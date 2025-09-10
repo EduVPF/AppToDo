ac840cfdca7ad3f75a51212274221ecaf1875ba0
Eduardo Vieira Ponce <duduemici23@gmail.com>
2025-09-10 13:53:42 -0300

    Update and rename AtividadeServico.java to TarefaServico.java
    Changing the name

--- ToDoListAtividade/src/ucb/aplicacao/service/TarefaServico.java
+++ ToDoListAtividade/src/ucb/aplicacao/service/TarefaServico.java
@@ -5,27 +5,27 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
-public class AtividadeServico {
+public class TarefaServico {
 
-    private final List<Atividade> bancoDeTarefas = new ArrayList<>();
+    private final List<Tarefa> bancoDeTarefas = new ArrayList<>();
     private int proximoId = 1;
 
-    private Optional<Atividade> buscarPorId(int id) {
+    private Optional<Tarefa> buscarPorId(int id) {
         return bancoDeTarefas.stream().filter(t -> t.getId() == id).findFirst();
     }
 
     public void criar(String titulo, String descricao) {
-        bancoDeTarefas.add(new Atividade(proximoId++, titulo, descricao));
+        bancoDeTarefas.add(new Tarefa(proximoId++, titulo, descricao));
     }
 
-    public List<Atividade> listar() {
+    public List<Tarefa> listar() {
         return new ArrayList<>(bancoDeTarefas);
     }
 
     public boolean atualizar(int id, String novoTitulo, String novaDescricao) {
         Optional<Atividade> tarefaOpt = buscarPorId(id);
         if (tarefaOpt.isPresent()) {
-            Atividade tarefa = tarefaOpt.get();
+            Tarefa tarefa = tarefaOpt.get();
             tarefa.setTitulo(novoTitulo);
             tarefa.setDescricao(novaDescricao);
             return true;
@@ -38,11 +38,12 @@ public class AtividadeServico {
     }
 
     public boolean marcarStatus(int id, boolean status) {
-        Optional<Atividade> tarefaOpt = buscarPorId(id);
+        Optional<Tarefa> tarefaOpt = buscarPorId(id);
         if (tarefaOpt.isPresent()) {
             tarefaOpt.get().setCompleta(status);
             return true;
         }
         return false;
     }
+
 }
